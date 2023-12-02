7de6c896c16b15dd94043b47fa6c2ae2ef74044c
ghariza <pranayaghariza@gmail.com>
2023-12-02 10:03:38 +0700

    fix errors

--- controller/getUsers.js
+++ controller/getUsers.js
@@ -1,8 +1,8 @@
-const Users = require("../models/Users");
+const Users = require("../models/Users.js");
 
 const getUsers = async (req, res) => {
   try {
-    const response = await User.findAll();
+    const response = await Users.findAll();
     res.status(200).json(response);
   } catch (error) {
     console.log(error.message);
