106c165483f63524f2f447ad6d9db4b6a55a54bc
daffaYuharshu <daffayuharshu@gmail.com>
2023-12-08 23:09:42 +0700

    small change

--- controller/getActivities.js
+++ controller/getActivities.js
@@ -4,19 +4,6 @@ const getActivities = async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
   const userId = req.cookies.userId;
   if(!refreshToken) return res.status(401);
-  
-  const activitiesIsExist = Activities.findAll({
-    where: {
-      user_id: userId
-    }
-  });
-
-  if(!activitiesIsExist){
-    return res.status(404).json({
-      "error": true,
-      "message": "Activity not found"
-  });
-  };
 
   try {
     const activities = await Activities.findAll({
@@ -24,6 +11,13 @@ const getActivities = async (req, res) => {
         user_id: userId
       }
     });
+    if(!activities){
+      return res.status(404).json({
+        "error": true,
+        "message": "Activity not found"
+      });
+    };
+
     res.status(200).json(activities);
   } catch (error) {
     res.status(500).json({
--- controller/getActivitiesById.js
+++ controller/getActivitiesById.js
@@ -6,32 +6,26 @@ const getActivitiesById = async (req, res) => {
     const activityId = req.params.id;
     if(!refreshToken) return res.status(401);
 
-    const activityIsExist = await Activities.findOne({
-      where: {
-          id: activityId
-      }
-    });
-
-    if (!activityIsExist) {
-        return res.status(404).json({
-            "error": true,
-            "message": "Activity not found"
-        });
-    };
-
-    if(userId !== activityIsExist.user_id) {
-      return res.status(401).json({
-        "error": true,
-        "message": "Unauthorized"
-      });
-    };
-
     try {
       const activities = await Activities.findOne({
         where: {
           id: activityId
         }
       });
+
+      if (!activities) {
+        return res.status(404).json({
+            "error": true,
+            "message": "Activity not found"
+        });
+      };
+
+      if(userId !== activities.user_id) {
+        return res.status(401).json({
+          "error": true,
+          "message": "Unauthorized"
+        });
+      };
       res.status(200).json(activities);
     } catch (error) {
       res.status(500).json({
